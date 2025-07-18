948d80af4eb3b22121fe67d57a5504670396b7b6
Roman <kaspryshyn_roman@ukr.net>
2025-07-18 12:18:16 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
-import Xxx from './components/Xxx';
-import Yyy from './components/Yyy';
+
 
 
 function App() {
   return (
     <div>
-     <Xxx place={'asdasd'}/>
-     <Xxx place='wwwww'/>
-     <Yyy />
-     
-
+  
+    
     </div>
   );
 }
