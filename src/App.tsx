97566fc99fba98dd75b201fd8d406b57cd960105
Roman <kaspryshyn_roman@ukr.net>
2025-07-18 23:25:06 +0300

    add useState , useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+interface User {
+  id:number;
+  name:string;
+}
 
 
 function App() {
+
+let [users ,setUsers] = useState<User[]>([])
+ useEffect(() => {
+ fetch('https://jsonplaceholder.typicode.com/users')
+ .then(value =>value.json())
+ .then(response =>{
+  setUsers(response);
+ 
+ 
+ })
+ },[])
+
+
   return (
     <div>
-  
+   
+   {
+    users.map(user =><div key={user.id}>{user.name}</div>)
+   }
     
     </div>
   );
