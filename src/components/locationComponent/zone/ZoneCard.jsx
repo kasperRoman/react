b96ae274bde1f9b2cfae74b2709d60bcead9e80d
Roman <kaspryshyn_roman@ukr.net>
2025-08-10 12:30:26 +0300

    asd

--- src/components/locationComponent/zone/ZoneCard.jsx
+++ src/components/locationComponent/zone/ZoneCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from '../Location.module.css'
 
+// style={{ backgroundImage: `url(${image})` }}
 const ZoneCard = ({title,image}) => {
   return (
-        <div className={styles.card} style={{ backgroundImage: `url(${image})` }}>
+        <div className={styles.card} >
+          <img src={image} alt={title} className={styles.image} />
       <div className={styles.overlay}>
         <h3 className={styles.cardTitle}>{title} </h3>
       </div>
